ba24618ec49021b6af5a47e4e9bfd5a969641764
Senih Tosun <38802705+senihtosun@users.noreply.github.com>
2020-11-05 13:13:50 -0500

    Create index.js

---
+++ static/index.js
@@ -0,0 +1,90 @@
+// Function will run everytime page refreshes
+// Sports category is the default to display
+
+let test = 0;
+
+$(document).ready(function() {
+  retrieveNews('sports')
+  test++
+  console.log(test)
+})
+
+// Sports function
+function sports(){
+  retrieveNews('sports')
+}
+
+// Technology function
+function tech(){
+  retrieveNews('technology')
+}
+
+// Entertainment function
+function entertainment(){
+  retrieveNews('entertainment')
+}
+
+
+// Main Function to fetch and append the news to html div
+function retrieveNews(category){
+  const apiKey = '78b9d599c4f94f8fa3afb1a5458928d6'
+
+  // Url to fetch the data by providing a category from the API
+  let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=30&apiKey=${apiKey}`
+
+  // Output variable to append all the news
+  let output = ""
+
+  // Request the url first
+  const request = new Request(url)
+  
+  // Fetch the request
+  fetch(request).then(response => response.json())
+    .then((news) => {
+
+      // For loop to go through each article one by one
+      // to be able to access title, author, imageUrl etc.
+      for (let i in news.articles){
+
+        // Turn the date string into a meaningful string
+        let date = new Date(news.articles[i].publishedAt)
+        let publishDate = date.toDateString()
+
+        // Concatenate everyting to the output variable
+        // All the styling is done by materialize css cards
+        output += `
+        <div class="col l4 m6 s12">
+        <div class="card medium hoverable">
+          <div class="card-image">                         
+            <a href="${news.articles[i].url}" target="_blank"><img src="${news.articles[i].urlToImage}" alt="" onerror="this.src='static/alt.jpg'"></a>
+          </div>
+          <div class="card-content">
+            <h6 class="truncate"><a href="${news.articles[i].url}" target='_blank' title="${news.articles[i].title}">${news.articles[i].title}</a></h6>
+            <p><b>Author</b>: ${news.articles[i].author}</p>
+            <p><b>Source</b>: ${news.articles[i].source.name}</p>
+            <p><b>Published</b>: ${publishDate} </p>
+          </div>
+
+          <div class="card-reveal">
+            <span class="card-title"><i class="material-icons right">close</i></span>
+            <p><b>Description</b>: ${news.articles[i].description}</p>
+          </div>
+
+          </div>
+        </div>
+        `   
+      }
+
+      // If the output variable is not empty display it
+      if (output !== ""){
+          $('#newsList').html(output)
+      }
+      
+      })
+  
+  // Catch and print to console if there are any errors while fetching
+  .catch(error => {
+      console.log(error)
+  })
+}
+
